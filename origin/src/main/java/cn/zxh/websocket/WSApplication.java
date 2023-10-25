d7b789fd4275cbb49f917e5f5f0a5d593d054ad8
zhanghao <2919730573@qq.com>
2023-10-25 11:28:00 +0800

    原生websocket配置

---
+++ origin/src/main/java/cn/zxh/websocket/WSApplication.java
@@ -0,0 +1,15 @@
+package cn.zxh.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanghao
+ * @create 2023-10-25-11:25
+ */
+@SpringBootApplication
+public class WSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WSApplication.class, args);
+    }
+}
